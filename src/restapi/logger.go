d64b573d4db633e027f1ec9bbf62db46c2f5d4c4
Ekaterina <katucha00@mail.ru>
2024-04-10 20:17:52 +0300

    working

--- src/restapi/logger.go
+++ src/restapi/logger.go
@@ -3,16 +3,22 @@ package openapi
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
+	logFile, err := os.OpenFile("./logger/logger.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed to open log file: %v", err)
+	}
+	logger := log.New(logFile, "", log.LstdFlags)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
 		inner.ServeHTTP(w, r)
 
-		log.Printf(
+		logger.Printf(
 			"%s %s %s %s",
 			r.Method,
 			r.RequestURI,
